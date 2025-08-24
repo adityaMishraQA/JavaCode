91ed53a5b93a4a9181cc68f5748f5d7859cffb23
Aditya Mishra <im.aditya707@gmail.com>
2025-08-24 17:35:28 +0530

    Adding new Branch

--- src/main/java/Try.java
+++ src/main/java/Try.java
@@ -2,5 +2,6 @@ public class Try {
     public static void main(String[] args) {
         System.out.println("noob");
         System.out.println("New add");
+        System.out.println("Adding new Branch");
     }
 }
